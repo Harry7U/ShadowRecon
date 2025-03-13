f358eff19601b23a99b4111542869babebfb29b1
Harindhranath Reddy <anonallin1@gmail.com>
2025-03-14 02:04:12 +0530

    Update live_scan.py

--- modules/live_scan.py
+++ modules/live_scan.py
@@ -3,7 +3,6 @@ import os
 def find_live_hosts(subdomains_file, config):
     output_file = "subdomains_alive.txt"
     print("[+] Scanning for live hosts...")
-    # Use httpx/httprobe to find live hosts.
     os.system(f"cat {subdomains_file} | httpx -ports 80,443,8080,8000,8888 -threads 200 > {output_file}")
     print(f"[+] Live hosts saved to {output_file}")
     return output_file
