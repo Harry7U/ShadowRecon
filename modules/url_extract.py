60748c80eddec7fff15131a5020110b1310fd8c1
Harindhranath Reddy <anonallin1@gmail.com>
2025-03-14 03:22:40 +0530

    Update url_extract.py

--- modules/url_extract.py
+++ modules/url_extract.py
@@ -4,7 +4,7 @@ def extract_data(active_hosts_file, config):
     urls_file = "allurls.txt"
     js_file = "js.txt"
     print("[+] Extracting URLs and JavaScript files...")
-    os.system(f"katana -u {active_hosts_file} -d 5 -ps -pss waybackarchive,commoncrawl,alienvault -kf -jc -fx -ef -o {urls_file}")
+    os.system(f"katana -u {active_hosts_file} -d 5 -silent -pss waybackarchive,commoncrawl,alienvault -kf -jc -fx -ef -o {urls_file}")
     os.system(f"cat {urls_file} | grep -E '\\.js$' >> {js_file}")
     print(f"[+] URLs saved to {urls_file} and JavaScript files to {js_file}")
     return urls_file, js_file
