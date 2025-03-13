9d3882d212d6308e7ba65b51bc635f0b8c76b07b
Harindhranath Reddy <anonallin1@gmail.com>
2025-03-14 04:01:22 +0530

    Update url_extract.py

--- modules/url_extract.py
+++ modules/url_extract.py
@@ -3,8 +3,10 @@ import os
 def extract_data(active_hosts_file, config):
     urls_file = "allurls.txt"
     js_file = "js.txt"
+    sensitive_files = "sensitive_files.txt"
     print("[+] Extracting URLs and JavaScript files...")
-    os.system(f"katana -u {active_hosts_file} -d 5 -silent -kf -jc -fx -ef -o {urls_file}")
-    os.system(f"cat {urls_file} | grep -E '\\.js$' >> {js_file}")
-    print(f"[+] URLs saved to {urls_file} and JavaScript files to {js_file}")
-    return urls_file, js_file
+    os.system(f"katana -list {active_hosts_file} -d 5 -ps -pss waybackarchive,commoncrawl,alienvault -kf -jc -fx -ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg -o {urls_file}")
+    os.system(f"cat {urls_file} | grep -E '\\.js$' > {js_file}")
+    os.system(f"cat {urls_file} | grep -E '\\.txt|\\.log|\\.cache|\\.secret|\\.db|\\.backup|\\.yml|\\.json|\\.gz|\\.rar|\\.zip|\\.config' > {sensitive_files}")
+    print(f"[+] URLs saved to {urls_file}, JavaScript files to {js_file}, and sensitive files to {sensitive_files}")
+    return urls_file, js_file, sensitive_files
